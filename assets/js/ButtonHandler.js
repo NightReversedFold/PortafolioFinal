ae06a4cec57e3830fc212279c918fda380e64afc
NightReversedFold <angel74977@gmail.com>
2025-09-26 22:39:24 -0600

    x

--- assets/js/ButtonHandler.js
+++ assets/js/ButtonHandler.js
@@ -3,7 +3,7 @@ const discordButton = document.getElementById('DiscordBt')
 discordButton.addEventListener("click", async () => {
     try {
         await navigator.clipboard.writeText('night_gsub');
-        alert('Discord copiado al portapapeles; presiona Ctrl + v para pegarlo!');
+        alert('Discord copiado al portapapeles; presiona Ctrl + v para pegarlo si estás en computadora!');
     } catch (err) {
         console.error('Error: ', err);
         alert('Error al copiar el Discord.');
